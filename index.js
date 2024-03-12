2969f79b742808ba8df1ea11ecc669f4b6a48269
Rizwanishere <rizwantopper@gmail.com>
2024-03-12 16:09:08 +0530

    Added URLs in nodejs(Routing)

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 // Import the Http Module
 const http = require('http');
+const fs = require('fs');
 
 /* Create a HTTP server instance using the 'createServer' 
-method and passing a request handler*/
+method and passing a request handler */
 const server = http.createServer(handler);
 
 /* Define the request handler function which responds 
 with the text "Hello nodeJs" to any incoming request*/
 function handler(req,res){
-    res.write("Hello nodeJS");
-    res.end();
+    switch(req.url){
+        // In case no url has been entered
+        case '/':
+            // readFileSync() is a synchronous method provided by the fs module in Node.js,used to read the contents of a file synchronously.
+            const contents = fs.readFileSync('index.html');
+            // toString() is used because readFileSync() returns file contents as a buffer or string
+            res.write(contents.toString());
+            res.end();
+            break;
+        
+        case '/books':
+            // we are passing objects in an array
+            const books = [{
+                id: 1,
+                name: 'Psychology of Money',
+                rating: 4,
+                author: 'Morgan Housel',
+                price: 300  
+            }, {
+                id: 2,
+                name: 'Rich Dad Poor Dad',
+                rating: 4.5,
+                author: 'Robert Kiyosaki',
+                price: 200
+            }];
+            // JSON.stringify() is specifically for converting JavaScript objects to JSON strings, not file contents.   
+            res.write(JSON.stringify(books));
+            res.end();
+            break;
+
+        case '/authors':
+            const authors = ['Morgan Housel','Robert Kiyosaki'];
+            
+            res.write(JSON.stringify(authors));
+            res.end();
+            break;
+        
+        default:
+            res.write('Not Found');
+            res.end();
+            break;
+    }
 }
 
 /* Specify the port number on which the server 
@@ -18,8 +59,6 @@ const port = 3000;
 
 /* Start the server to listen for incoming 
 requests on the specified port */
-server.listen(port,()=>{
-    console.log(`The server is running on ${port}`);
-});
+server.listen(port,()=> console.log(`The server is running on ${port}`));
 
 
