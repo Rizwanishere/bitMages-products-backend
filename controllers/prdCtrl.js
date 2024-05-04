5168dbbb1760374f894b81321856e0c9f8586447
Rizwanishere <rizwantopper@gmail.com>
2024-05-04 16:19:47 +0530

    Avg rating using aggregation pipeline

--- controllers/prdCtrl.js
+++ controllers/prdCtrl.js
@@ -70,7 +70,9 @@ const getById = async(req,res) => {
         res.status(404).send('Not found');
     }else{
         const reviews = await reviewRepo.get(id);
-        const response = {...data._doc,reviews};
+        const avgRating = await reviewRepo.getAvgRating(id);
+        const Ratings = avgRating.length > 0 ? avgRating[0].avg : undefined;
+        const response = {...data._doc,Ratings,reviews};
         res.status(200).json(response);
     }
 }
--- repositories/reviewRepo.js
+++ repositories/reviewRepo.js
@@ -9,7 +9,17 @@ const get = (productId) => {
     return Review.find({productId:productId},{__v:0,_id:0});
 };
 
+const getAvgRating = (productId) => {
+    return Review.aggregate(
+        [
+            {$match: {productId:productId}},
+            {$group: {_id:'$productId',avg:{$avg:'$rating'}}}
+        ]
+    );
+};
+
 module.exports = {
     add,
     get,
+    getAvgRating
 };
